5df8de41fbf60153d3fd89ede26d5d02242f4a65
th4r1k <th4r1k@gmail.com>
2022-07-01 13:51:56 -0300

    style: change button color

--- components/closebutton.tsx
+++ components/closebutton.tsx
@@ -4,7 +4,7 @@ import { X } from 'phosphor-react'
 export function CloseButton() {
   return (
     <Popover.Button
-      className="absolute top-5 right-5 text-zinc-400 hover:text-zinc-100"
+      className="absolute top-5 right-5 text-zinc-400 hover:text-zinc-100 dark:text-black"
       title="Fechar formulário"
     >
       <X weight="bold" className="h-4 w-4" />
